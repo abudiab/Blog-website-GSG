a466e2c98ebb84cd2d20f7d038e0aa39d74bf0f5
Mahmoud Diab <mahmoud.w.diab@gmail.com>
2023-05-22 00:03:45 +0300

    creating pages files and routing to them

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,34 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Homepage from "./pages/Homepage";
+import Error from "./pages/Error";
+import Edit from "./pages/Edit";
+import Article from "./pages/Article";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is the Homepage</div>,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
-  return <div>test</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
