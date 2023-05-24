fba2a085ed68a4dd3a65d72542bd21ccdf704478
Mahmoud Diab <mahmoud.w.diab@gmail.com>
2023-05-24 21:18:10 +0300

    created the registration page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="auth">
+      <h1>Sign up</h1>
+      <form>
+        <input required type="text" placeholder="username"></input>
+        <input required type="email" placeholder="email"></input>
+        <input required type="password" placeholder="password"></input>
+        <button>Register</button>
+        <p>Error message placeholder</p>
+        <span>
+          Already have an account? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
