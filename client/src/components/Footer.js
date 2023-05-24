4e3e1a2f27e3596b9b359a667589e568a28a42fe
Mahmoud Diab <mahmoud.w.diab@gmail.com>
2023-05-24 23:34:42 +0300

    initial footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,25 @@
 import React from "react";
+import Logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>Made with ❤️ in Palestine.</span>
+      <div className="links">
+        <Link className="link" to="/">
+          <h6>Home</h6>
+        </Link>
+        <Link className="link" to="/about">
+          <h6>About</h6>
+        </Link>
+        <Link className="link" to="/contact">
+          <h6>Contact</h6>
+        </Link>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
