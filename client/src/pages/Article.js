5b07dd60450815da1e92d0120daa291b5135426e
Mahmoud Diab <mahmoud.w.diab@gmail.com>
2023-05-26 19:28:07 +0300

    single article page initial with placeholder text

--- client/src/pages/Article.js
+++ client/src/pages/Article.js
@@ -1,7 +1,62 @@
 import React from "react";
+import Edit from "../assets/edit.png";
+import Delete from "../assets/delete.png";
+import { Link } from "react-router-dom";
 
 const Article = () => {
-  return <div>Article</div>;
+  return (
+    <div className="single">
+      <div className="content">
+        <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
+        <div className="user">
+          <img src="https://i.pravatar.cc/160?img=8" alt="" />
+
+          <div className="info">
+            <span>John</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            <Link to={`/edit?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+
+            <img src={Delete} alt="" />
+          </div>
+        </div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus
+          excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem
+          ratione sit debitis deserunt repellendus numquam ab vel perspiciatis
+          corporis!
+        </p>
+        <br />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus
+          excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem
+          ratione sit debitis deserunt repellendus numquam ab vel perspiciatis
+          corporis!
+        </p>
+        <br />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus
+          excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem
+          ratione sit debitis deserunt repellendus numquam ab vel perspiciatis
+          corporis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. A
+          possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea
+          dolorem ratione sit debitis deserunt repellendus numquam ab vel
+          perspiciatis corporis! Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam
+          facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus
+          numquam ab vel perspiciatis corporis! Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. A possimus excepturi aliquid nihil
+          cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis
+          deserunt repellendus numquam ab vel perspiciatis corporis!
+        </p>
+      </div>
+      <div className="menu"></div>
+    </div>
+  );
 };
 
 export default Article;
