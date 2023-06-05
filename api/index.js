9114447422762818e82013809c9cf7a0998ce336
Mahmoud Diab <mahmoud.w.diab@gmail.com>
2023-06-05 06:10:36 +0300

    fixed typo

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const PORT = 5500;
 const app = express();
 
-app.unsubscribe(express.json());
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Dancing on port ${PORT}`);
